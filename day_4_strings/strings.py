94501677f74a2d3501d8b3903e5511c228db2179
Vasudha Rastogi <vasudhaa.rastogi@ymail.com>
2026-07-01 10:10:24 +0200

    Remove introductory Python exercises

---
+++ day_4_strings/strings.py
@@ -0,0 +1,16 @@
+# concatenation
+string_1 = 'Thirty' + 'Days' + 'Of' + 'Python'
+string_2 = 'Coding' + ' ' + 'For' + ' ' + 'All'
+company = string_2
+
+# Print the variable company using print().
+print(company)
+print(len(company))
+print(company.upper())
+print(company.lower())
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
+
+last_two = company[7::]
+print(last_two)
